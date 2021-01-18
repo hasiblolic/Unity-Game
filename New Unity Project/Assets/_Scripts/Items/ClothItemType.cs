8b0afb0196ba1220c8912413dec34e218cb499b1
Hasib Lolic <hasiblolic@gmail.com>
2021-01-18 10:38:04 -0500

    removed old project

--- New Unity Project/Assets/_Scripts/Items/ClothItemType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Items/Cloth Item Type")]
-public class ClothItemType : ScriptableObject
-{
-    public bool isDisabledWhenNoItem;
-}
--- New Unity Project/Assets/_Scripts/Utilities/Interfaces.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface ILockable
-{
-    Transform GetLockOnTarget(Transform from);
-}
